48dbe97315cec894a5523c706214daf950c10c00
Munkhtegsh <munkhtegsh@outlook.com>
2019-02-14 22:47:31 -0800

    Connected db serializer

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 let express = require('express');
 let bodyParser = require('body-parser');
+let massive = require('massive');
 
 let app = express();
-
 app.use(bodyParser.json());
 
+massive(process.env.DB_URI).then(instance => {
+  console.log('Massive attack!');
+  app.set('db', instance);
+});
 let PORT = 4000;
 app.listen(PORT, () => console.log(`App is running on: ${PORT}`));
