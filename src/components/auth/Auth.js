195319c893ab85283cb798458239e57d6aeeeda9
Munkhtegsh <munkhtegsh@outlook.com>
2019-02-17 22:47:53 -0800

    Input changes state

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
 
 class Auth extends Component {
+  state = {
+    username: '',
+    password: '',
+  };
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
     console.log(this.props.location.pathname);
     return (
       <div>
         {this.props.location.pathname === '/' ? null : (
           <div>
-            <input placeholder="username" />
-            <input placeholder="password" />
+            <input
+              name="username"
+              value={this.state.username}
+              placeholder="username"
+              onChange={e => this.handleInput(e)}
+            />
+            <input
+              name="password"
+              value={this.state.password}
+              placeholder="password"
+              onChange={e => this.handleInput(e)}
+            />
             <button> Login </button>
             <button> Register </button>
           </div>
