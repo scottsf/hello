bd3b078fe2bea291965a1bf20634de7850d43c3e
Munkhtegsh <munkhtegsh@outlook.com>
2019-02-21 23:30:55 -0800

    Backend responds with user id when user login

--- server/controller.js
+++ server/controller.js
@@ -4,6 +4,14 @@ module.exports = {
     const db = req.app.get('db');
     db.registerUser([username, password]).then(instance => {
       console.log(instance);
+      res.status(200).send(instance);
+    });
+  },
+  loginUser: (req, res) => {
+    const {username, password} = req.body;
+    console.log('here');
+    const db = req.app.get('db');
+    db.loginUser([username, password]).then(instance => {
       res.status(200).send(instance[0]);
     });
   },
