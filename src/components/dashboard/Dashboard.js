5f7359084837edaa74a19f1602e74d249195ff7e
Munkhtegsh <munkhtegsh@outlook.com>
2019-02-22 21:51:37 -0800

    Dashboard renders posts

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,38 @@
 import React, {Component} from 'react';
 
 class Dashboard extends Component {
+  state = {
+    input: '',
+    myPosts: true,
+    posts: [
+      {username: 'Potter', title: 'Secret spell', profilePicture: 'https:dfsf'},
+    ],
+  };
+
+  toggleMyPost = () => {
+    this.setState({myPosts: !this.state.myPosts});
+  };
+
   render() {
-    return <div>Dashboard</div>;
+    const posts = this.state.posts.map(post => (
+      <p key={post.id}>
+        {post.username} {post.title} {post.profilePicture}
+      </p>
+    ));
+    return (
+      <div>
+        <input onChange={e => this.setState({input: e.target.value})} />
+        <button>Search</button>
+        <button onClick={() => this.setState({input: ''})}>Reset</button>
+        <input
+          type="checkbox"
+          label="My Posts"
+          value={this.state.myPosts}
+          onClick={() => this.toggleMyPost()}
+        />
+        {posts}
+      </div>
+    );
   }
 }
 
